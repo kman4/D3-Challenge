7d8af94f57fa4e85dd3dc73a364112a69cf8384e
Kapil Pundhir <70111855+kman4@users.noreply.github.com>
2021-01-27 21:42:47 -0500

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,11 @@
-var svgHeight = 800;
-var svgWidth = 600;
+var svgHeight = 460;
+var svgWidth = 400;
 // Defining the Chart's margins as an object
 var chartMargin = {
-    top: 30,
+    top: 10,
     right: 30,
     bottom: 30,
-    Left:30
+    Left:60
 };
 
 var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
@@ -17,37 +17,47 @@ var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
 
 var svg = d3.select("#scatter")
     .append("svg")
-    .attr("height",svgHeight)
-    .attr("width",svgWidth);
+    .attr("height",svgHeight + chartMargin.top + chartMargin.bottom)
+    .attr("width",svgWidth + chartMargin.top + chartMargin.right)
+    .append("g")
+    .attr("transform", "translate(" + chartMargin.left + "," + chartMargin.top +")");
 
-// Append an SVG group
+// reading data
 
-var chartGroup = svg.append("g")
-    .attr("transform", `translate(${chartMargin.left},${chartMargin.top})`)
-
-d3.csv('./data.csv').then(function(data){
+    d3.csv("./data.csv").then(function(data){
     var poverty = data.map( d => d.poverty);
     var healthcare = data.map( d=> d.healthcare);
-    
-    var xScale = d3.scaleBand()
-                    .domain(poverty)
-                    .range([0,chartWidth]
-                    .padding(0.1)
+// Add X Scale
+    var xScale = d3.scaleLinear()
+                    .domain(5,poverty)
+                    .range([0,chartWidth]);
+    svg.append("g").attr("transform", "translate(0," + svgHeight + ")")
+                  .call(d3.axisBottom(xScale));
+                        
+// Add Y Scale
+        
 
-    var yScale = d3.scaleBand()
-                   .domain([0, d3.max(healthcare)])
+    var yScale = d3.scaleLinear()
+                   .domain([28, d3.max(healthcare)])
                    .range([chartHeight,0])
-    var yAxis = d3.axisLeft(yScale)
-    var xAxis = d3.axisBottom(xScale)
+    svg.append("g").call(d3.axisLeft(yScale));
 
-// set the x axis to the bottom of the chart
-chartGroup.append("g")
-     .attr("transform", 'translate(0)${chartHeight})')
-     .call(xAxis)
+ // Add dots
+    svg.append('g')
+       .selectAll("dot")
+       .data(data)
+       .enter()
+       .append("circle")
+       .attr("cx", function (d) { return x(poverty); } )
+       .attr("cy", function (d) { return y(healthcare); } )
+       .attr("r", 5)
+       .style("fill", "#69b3a2")
 
-//  set the Y axis
-   chartGroup.append("g")
-    .attr("transform", 'translate(0)${chartHeight})')
-    .call(yAxis)               
-})
+
+
+
+
+
+
+});
   
